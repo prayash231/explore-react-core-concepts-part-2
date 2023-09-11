f45380cac1b4f43e2d2cc905cba43d19283cc12d
prayash231 <138357568+prayash231@users.noreply.github.com>
2023-09-11 13:09:19 +0600

    explore event, state, use-effect

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Counter from './Counter'
+import Team from './Team'
+import Users from './Users'
+import Friends from './Friends'
+
+function App() {
+
+  function handleClick(){
+    alert('button clicked');
+  }
+
+  const handleClick2 = () => {
+    alert('button 2 clicked');
+  }
+
+  const addToFive = (num) => {
+    alert(num + 5);
+  }
+
+  return (
+    <>
+      <h3>React Core Concepts 2</h3>
+      <Friends></Friends>
+      <Users></Users>
+      <Team></Team>
+      <Counter></Counter>
+      <button onClick = {handleClick}>Click Me</button>
+      <button onClick = {handleClick2}>Click2</button>
+      <button onClick = {() => {alert('third clicked')}}>third</button>
+      <button onClick = {() => addToFive(5)}>Four</button>
+    </>
+  )
+
+}
+
+export default App
